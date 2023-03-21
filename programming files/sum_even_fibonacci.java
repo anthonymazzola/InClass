791325ee57cf3b5617d6f30032eb9da45c011017
anthonymazzola <112509417+anthonymazzola@users.noreply.github.com>
2023-03-21 10:22:21 -0400

    Added extensive comments

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,7 +1,10 @@
 public class sum_even_fibonacci {
     public static void main(String[] args) {
+        // Initializing variables
         int a = 0, b = 1;
         int sumEven = 0;
+        
+        // While loop to sum even fibonacci
         while (b < 4000000) {
             if (b % 2 == 0) {
                 sumEven += b;
